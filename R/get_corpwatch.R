2c8833c70ea6b4e2dd3eafacab7e249b8d14383f
erikbjohn <erikbjohn@gmail.com>
2018-06-28 11:19:47 -0500

    Demo commit

--- R/get_corpwatch.R
+++ R/get_corpwatch.R
@@ -1,4 +1,5 @@
 get_corpwatch <- function(){
+  # Try a commit
   corpwatch_location <- '~/Dropbox/pkg.data/relocation/raw/corpwatch.csv.tar.gz'
   if(!(file.exists(corpwatch_location))){
     csv <- download.file('http://api.corpwatch.org/documentation/db_dump/corpwatch_api_tables_csv.tar.gz', corpwatch_location)
